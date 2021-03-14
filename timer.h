d85c921bbe4f5267c0028e3387d95f28dc6fac41
viklo <viggo@gstromberg.nu>
2021-03-14 19:39:02 +0100

    header-only timer

--- timer.h
+++ timer.h
@@ -11,14 +11,26 @@ private:
 	double waitTime;
 	double delta;
 public:
-	Timer(double framerate) noexcept;
+	Timer(double framerate) noexcept :
+		waitTime(1.0 / framerate),
+		delta(waitTime),
+		frameStartTime(TimerClock::now()) {}
 
 	Timer(const Timer&) = delete;
 	Timer& operator=(const Timer&) = delete;
 
 	void setFramerate(double framerate) noexcept {waitTime = 1.0 / framerate;}
 
-	void wait();
+	void wait()
+	{
+		const auto frameEndTime = TimerClock::now();
+		const double d = (frameEndTime - frameStartTime).count() / 1000000000.0;
+
+		std::this_thread::sleep_for(std::chrono::duration<double>(waitTime - d));
+		const auto prevFrameStartTime = frameStartTime;
+		frameStartTime = TimerClock::now();
+		delta = (frameStartTime - prevFrameStartTime).count() / 1000000000.0;
+	}
 
 	double getDelta() const noexcept {return delta;}
 	double getFramerate() const noexcept {return 1.0 / delta;}
@@ -30,9 +42,16 @@ private:
 	std::chrono::time_point<TimerClock> frameStartTime;
 	double delta;
 public:
-	NoWaitTimer() noexcept;
+	NoWaitTimer() noexcept :
+		delta(0.0),
+		frameStartTime(TimerClock::now()) {}
 
-	void endFrame();
+	void endFrame()
+	{
+		const auto prevFrameStartTime = frameStartTime;
+		frameStartTime = TimerClock::now();
+		delta = (frameStartTime - prevFrameStartTime).count() / 1000000000.0;
+	}
 
 	double getDelta() const noexcept {return delta;}
 	double getFramerate() const noexcept {return 1.0 / delta;}
